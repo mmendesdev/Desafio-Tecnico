1b804dd8de4efebd7b5454e03b7d99adda33cb5c
mmendesdev <matheusmendesbmf@outlook.com>
2025-08-05 01:41:48 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# backend/main.py
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="API de Formulários Dinâmicos",
+    description="API para gerenciar formulários e perguntas dinâmicas.",
+    version="0.0.1",
+)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Bem-vindo à API de Formulários Dinâmicos!"}
